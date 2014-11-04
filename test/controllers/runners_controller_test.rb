61d824d17ae1587404b62f246865e419b4c2cc98
Zac Wasielewski <zac@wasielewski.org>
2014-11-04 11:13:15 -0500

    Add unit tests for data import and strip out unnecessary scaffold tests

--- test/controllers/runners_controller_test.rb
+++ test/controllers/runners_controller_test.rb
@@ -11,39 +11,9 @@ class RunnersControllerTest < ActionController::TestCase
     assert_not_nil assigns(:runners)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create runner" do
-    assert_difference('Runner.count') do
-      post :create, runner: { age_group: @runner.age_group, country: @runner.country, gun_pace: @runner.gun_pace, gun_time: @runner.gun_time, name: @runner.name, net_pace: @runner.net_pace, net_time: @runner.net_time, place: @runner.place, place_in_age_group: @runner.place_in_age_group, place_in_sex: @runner.place_in_sex, sex: @runner.sex, state: @runner.state }
-    end
-
-    assert_redirected_to runner_path(assigns(:runner))
-  end
-
   test "should show runner" do
     get :show, id: @runner
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: @runner
-    assert_response :success
-  end
-
-  test "should update runner" do
-    patch :update, id: @runner, runner: { age_group: @runner.age_group, country: @runner.country, gun_pace: @runner.gun_pace, gun_time: @runner.gun_time, name: @runner.name, net_pace: @runner.net_pace, net_time: @runner.net_time, place: @runner.place, place_in_age_group: @runner.place_in_age_group, place_in_sex: @runner.place_in_sex, sex: @runner.sex, state: @runner.state }
-    assert_redirected_to runner_path(assigns(:runner))
-  end
-
-  test "should destroy runner" do
-    assert_difference('Runner.count', -1) do
-      delete :destroy, id: @runner
-    end
-
-    assert_redirected_to runners_path
-  end
 end
--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class RaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @race = races(:one)
+  end
+
+  test "should import runners from text file" do
+		text = <<-EOS
+PLACE NAME                    SEX     &   AGEGROUP        TIME   PACE     TIME   PACE   RESIDENCE         
+------------------------------------------------------------------------------------------------------------
+    1 Geoffrey Kenisi Bundi  M#   1     1/934  M25-29     44:18  4:46     44:18  4:46   Kenya                
+    2 Julius Keter           M#   2     2/934  M25-29     44:33  4:47     44:32  4:47   Kenya                
+    3 Belete Assefa          M#   3     1/638  M20-24     44:44  4:48     44:43  4:48   Ethiopia             
+    4 Allan Kiprono          M#   4     2/638  M20-24     44:52  4:49     44:51  4:49   Kenya                
+    5 Samson Gebre. Gezahai  M#   5     3/638  M20-24     45:22  4:52     45:20  4:52   Eritrea              
+ 5312 Noah Hammond           M#3584   504/863  M30-34   1:36:50 10:24   1:32:26  9:55   Westport          CT 
+ 5313 Zachary Wasielewski    M#3585   505/863  M30-34   1:36:51 10:24   1:34:50 10:11   Utica             NY 
+ 5314 Andra L. Kowalczyk     F#1729   324/1022 F25-29   1:36:51 10:24   1:26:06  9:15   Columbia          MD 
+EOS
+
+		file = Tempfile.new('new_runners.txt')
+		file.write(text)
+		file.rewind
+
+		assert_difference "Runner.count", 8 do
+			@race.import_runners_from_file( Rack::Test::UploadedFile.new(file, 'text/txt') )
+		end
+
+		updated_runner = @race.runners.find_by_place(1)
+		assert_equal "Geoffrey Kenisi Bundi", updated_runner.name
+		
+		new_runner = @race.runners.find_by_place(5)
+		assert_equal "Samson Gebre. Gezahai", new_runner.name
+
+		global_runner = @race.runners.find_by_place(3)
+		assert_equal "Ethiopia", global_runner.country
+		assert_equal nil, global_runner.state
+		assert_equal nil, global_runner.city
+		
+		local_runner = @race.runners.find_by_place(5313)
+		assert_equal "United States", local_runner.country
+		assert_equal "NY", local_runner.state
+		assert_equal "Utica", local_runner.city
+		
+  end
+
 end
