76e5b2164343010df2020533b16c83338dea4811
Zac Wasielewski <zac@wasielewski.org>
2014-11-05 16:10:10 -0500

    Refactor runner import code

--- app/models/race.rb
+++ app/models/race.rb
@@ -3,40 +3,18 @@ class Race < ActiveRecord::Base
 	has_many :runners
 	
   def import_runners_from_file(file)
-  	runners = parse_runners_from_file(file)
-    runners[:body].each do |runner_data|
-    	self.import_runner(runner_data)
-    end
-  end
   
-  def import_runner(r)
-		if r[:place] > 0
-			
-			r = normalize_runner_data(r)
-			
-			runner = Runner.find_or_initialize_by({:race_id => self.id, :place => r[:place]})
-			runner.update(
-				place: r[:place],
-				name: r[:name],
-				sex: r[:sex],
-				place_in_sex: r[:place_in_sex],
-				place_in_age_group: r[:place_in_age_group],
-				total_in_age_group: r[:total_in_age_group],
-				age_group: r[:age_group],
-				gun_time: r[:gun_time],
-				gun_pace: r[:gun_pace],
-				net_time: r[:net_time],
-				net_pace: r[:net_pace],
-				country: r[:country],
-				state: r[:state],
-				city: r[:city]
-			)
-			
-		end  	
+		ActiveRecord::Base.transaction do
+			parse_runners_from_file(file).each{ |runner_data|
+				runner = Runner.find_or_initialize_by({ :race_id => self.id, :place => runner_data[:place] })
+				runner.attributes = normalize_runner_data(runner_data)
+				runner.save
+			}
+		end
+  	
   end
-  
+    
 	def parse_runners_from_file(file)
-		
     Slither.define :runners, :by_bytes => false, :validate_length => false, force_character_offset: true do |d|
       d.header do |header|
     		header.trap { |line|
@@ -75,26 +53,37 @@ class Race < ActiveRecord::Base
         body.spacer 1
       end
     end
-    runners = Slither.parse(file,:runners)
+    Slither.parse(file,:runners)[:body]
 	end
   
-  def normalize_runner_data(runner_data)
-		location = normalize_location(runner_data[:city_or_country],runner_data[:state])
-		runner_data[:name] = runner_data[:name].force_encoding('UTF-8')
-		runner_data[:country] = location[:country]
-		runner_data[:state] = location[:state]
-		runner_data[:city] = location[:city]
-		runner_data
+  def normalize_runner_data(runner)
+		location = normalize_location(runner[:city_or_country],runner[:state])
+  	{
+			:place							=> runner[:place],
+			:name								=> runner[:name].force_encoding('UTF-8'),
+			:sex								=> runner[:sex],
+			:place_in_sex				=> runner[:place_in_sex],
+			:place_in_age_group => runner[:place_in_age_group],
+			:total_in_age_group => runner[:total_in_age_group],
+			:age_group					=> runner[:age_group],
+			:gun_time						=> runner[:gun_time],
+			:gun_pace						=> runner[:gun_pace],
+			:net_time						=> runner[:net_time],
+			:net_pace						=> runner[:net_pace],
+			:country						=> location[:country],
+			:state							=> location[:state],
+			:city								=> location[:city]
+		}
   end
   
   def normalize_location(city_or_country,state)
   	location = {}
 		if state.empty?
-			location[:country] = city_or_country.force_encoding('UTF-8') # Country.named(vals[:city_or_country]).code
+			location[:country] = city_or_country.force_encoding('UTF-8')
 			location[:state] = nil
 			location[:city] = nil
 		else
-			location[:country] = 'United States' # Country.named("United States").code
+			location[:country] = 'United States'
 			location[:state] = state.force_encoding('UTF-8')
 			location[:city] = city_or_country.force_encoding('UTF-8')
 		end
